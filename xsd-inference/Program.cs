649fd7bc2dc80e7b2e3502275f06078266184d1a
Casper Plentinger <c.plentinger@blinkdigital.nl>
2025-02-20 12:03:30 +0100

    Add XML to XSD inference script

---
+++ xsd-inference/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+
+namespace xsd_inference
+{
+    class XsdInference
+    {
+        static void Main(string[] args)
+        {
+            // Get all files in the XML directory
+            string[] files = Directory.GetFiles("XML", "*.xml");
+            
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "  ",
+                NewLineOnAttributes = false,
+                OmitXmlDeclaration = false
+            };
+            
+            foreach (var file in files)
+            {
+                XmlReader reader = XmlReader.Create(file);
+                XmlSchemaInference inference = new XmlSchemaInference();
+                XmlSchemaSet schemaSet = inference.InferSchema(reader);
+
+                int index = 0;
+                int numberOfSchemas = schemaSet.Schemas().Count;
+                foreach (XmlSchema schema in schemaSet.Schemas())
+                {
+                    string outputPath;
+                    if (numberOfSchemas == 1)
+                    {
+                        outputPath = $"XSD/{Path.GetFileNameWithoutExtension(file)}/result.xsd";
+                    }
+                    else
+                    {
+                        outputPath = $"XSD/{Path.GetFileNameWithoutExtension(file)}/result{index + 1}.xsd";
+                    }
+                    
+                    Directory.CreateDirectory(Path.GetDirectoryName(outputPath)!);
+                    
+                    using XmlWriter writer = XmlWriter.Create(outputPath, settings);
+                    schema.Write(writer);
+                    index++;
+                }
+            }
+        }
+    }
+}
